62ab0891ce2ce9d97a58747e3eeda7fc3cf3838c
alejandrox1 <alarcj137@gmail.com>
2018-05-11 10:13:59 -0400

    containers/desktop-apps/ubuntu-nvidia/run-unv.sh build and run ubuntu-nvidia image

---
+++ containers/desktop-apps/ubuntu-nvidia/run-unv.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+GRE="\e[32m"
+NOC="\e[0m"
+
+IMG="ubuntu-nvidia"
+TAG="docker.io/alejandrox1/ubuntu-nvidia:latest"
+
+if [[ "$#" == 1 && "$1" == "build" ]]; then
+    echo -e "${GRE}Building... Tagging... and Pushing image...${NOC}" && \
+        docker build -t "${IMG}" . && \
+        docker tag "${IMG}" "${TAG}" && \
+        docker push "${TAG}"
+fi
+
+docker run -it \
+    -v $(pwd):/home \
+    --device /dev/nvidia0:/dev/nvidia0 \
+    --device /dev/nvidiactl:/dev/nvidiactl \
+    --device /dev/nvidia-uvm:/dev/nvidia-uvm \
+    --device /dev/nvidia-uvm-tools:/dev/nvidia-uvm-tools \
+    ubuntu-nvidia /bin/bash
+
