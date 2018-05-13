098aa5958a5de91496b7aa88f28b1f23a933556c
alejandrox1 <alarcj137@gmail.com>
2018-05-12 20:32:38 -0400

    containers/desktop-apps/shellcheck/run-spellcheck.sh shellcheck container

---
+++ containers/desktop-apps/shellcheck/run-spellcheck.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# SHELLCHECK all *.sh files in the current working directory.
+#
+# Another good utility to eventually put along with this would be 'file'.
+#
+set -e
+
+GRE="\e[34;1;5m"
+NOC="\e[0m"
+
+IMG="shellchecker"
+TAG="docker.io/alejandrox1/${IMG}:latest"
+
+# Remove all exited shellcheck containers.
+docker ps -aq -f name=shellchecker* -f status=exited | xargs -r docker rm
+
+# Build your container.
+docker build -t "${IMG}" . 
+
+# If passed "push" then push a tagged image to a registry.
+if [[ $# == 1 && $1 == "push" ]]; then
+    echo -e "${GRE}Building... Tagging... and Pushing image...${NOC}" && \
+        docker tag "${IMG}" "${TAG}" && \
+        docker push "${TAG}"
+fi
+
+# Shellcheck all *.sh files in the current working directory.
+for sh in *.sh; do
+    echo -e "${GRE}Shellcheking ${sh}...${NOC}" && \
+        docker run --name "${IMG}-${sh}" \
+        -v $(pwd):/home \
+        shellchecker "${sh}"
+done
