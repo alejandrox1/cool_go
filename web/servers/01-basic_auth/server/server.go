26a3a371356529baa72cad73b8cf72888c7bb791
alejandrox1 <alarcj137@gmail.com>
2018-06-07 23:19:30 -0500

    web/servers/01-basic_auth/server/server.go server source

---
+++ web/servers/01-basic_auth/server/server.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"database/sql"
+    "log"
+	"net/http"
+
+    _ "github.com/lib/pq"
+)
+
+//var db *sql.DB
+
+func initDB() {
+    dataSource := getDBCreds("/vault/configs/vault.json")
+
+    _, err := sql.Open("postgres", dataSource)
+    if err != nil {
+        panic(err)
+    }
+}
+
+func main() {
+    initDB()
+
+    //http.HandleFunc("/singin", Signin)
+    //http.HandleFunc("/signup", Signup)
+
+    log.Fatal(http.ListenAndServe(":8000", nil))
+}
---
+++ web/servers/01-basic_auth/server/server_setup.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/hashicorp/vault/api"
+)
+
+type Vault struct {
+	Addr    string `json:"vault_address"`
+	Service string `json:"vault_service"`
+	Token   string `json:"vault_token"`
+}
+
+func readVaultConfig(filename string) (vaultConfig Vault, err error) {
+	contents, err := os.Open(filename)
+	if err != nil {
+		fmt.Printf("readVaultConfig error opening file: %s\n", err.Error())
+		return
+	}
+
+	err = json.NewDecoder(contents).Decode(&vaultConfig)
+	if err != nil {
+		fmt.Printf("readVaultConfig error decoding json: %s\n", err.Error())
+		return
+	}
+
+	return
+}
+
+func getDBCreds(configFile string) string {
+	vaultConfig, err := readVaultConfig(configFile)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	client, err := api.NewClient(&api.Config{
+        Address: "http://vault", //vaultConfig.Addr,
+	})
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	client.SetToken(vaultConfig.Token)
+
+	secretValues, err := client.Logical().Read(vaultConfig.Service)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	var creds strings.Builder
+	creds.WriteString(fmt.Sprintf("user=%s ", secretValues.Data["username"]))
+	creds.WriteString(fmt.Sprintf("password=%s ", secretValues.Data["password"]))
+	creds.WriteString("host=db dbname=db sslmode=disable")
+    return creds.String()
+}
