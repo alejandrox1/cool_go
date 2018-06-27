e5b8e9bb6243d1053e46457bbd9c77e8f546bb52
alejandrox1 <alarcj137@gmail.com>
2018-06-27 10:43:21 -0500

    containers/desktop-apps/git_dev/run.sh build and run your container
    Signed-off-by: alejandrox1 <alarcj137@gmail.com>

---
+++ containers/desktop-apps/git_dev/run.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#-
+#-                          :-) Testing sd2e-cli (-:
+#-
+#- Testing nvironment for the development of github.com/sd2e/sd2e-cli fork on
+#- github.com/alejandrox1/sd2e-cli.
+#-
+##
+## Usage:
+##
+##      ./run.sh [-v|-h] [-b|--branch]
+##
+##      -v|--version print to stdout any information related to this script.
+##
+##      -h|--help print to stdout any help information included in the header
+##                of the script.
+##
+##
+set -e
+set -o pipefail
+export blue="\e[1;34m"                                                         
+export green="\e[32m"                                                           
+export red="\e[1;31m"
+export reset="\e[0m"
+
+# Input parameters
+
+# Parse command line arguments.
+while [[ "$#" > 0 ]]; do
+    arg="$1"
+
+    case "${arg}" in
+        -v|--version)
+            echo "$(grep "^#-" ${BASH_SOURCE[0]} | cut -c 4-)"
+            exit 0
+            ;;
+        -h|--help)
+            echo "$(grep "^##" ${BASH_SOURCE[0]} | cut -c 4-)"
+            exit 0
+            ;;
+        *)
+            >&2 echo "Unknown command-line option: '${arg}'."
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+
+CONTAINER="sd2e-cli-dev"
+
+echo -e "${red}Starting development...${reset}"
+docker build --no-cache --force-rm \
+    --build-arg UID=$UID \
+    --build-arg USER=$USER \
+    --build-arg REPO=https://github.com/alejandrox1/sd2e-cli \ 
+    -t $CONTAINER . && \
+    docker run \
+    -v ~/.gitconfig:/home/$USER/.gitconfig \
+    -v ~/.git-credentials:/home/$USER/.git-credentials \
+    --rm -it $CONTAINER
