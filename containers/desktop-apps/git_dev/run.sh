6909a4960e4e39013ab43538bb42808a83ed8dab
alejandrox1 <alarcj137@gmail.com>
2018-06-27 11:14:03 -0500

    containers/desktop-apps/git_dev/run.sh generalized workflow
    Signed-off-by: alejandrox1 <alarcj137@gmail.com>

--- containers/desktop-apps/git_dev/run.sh
+++ containers/desktop-apps/git_dev/run.sh
@@ -15,6 +15,7 @@
 ##      -h|--help print to stdout any help information included in the header
 ##                of the script.
 ##
+##      --get-tar downloads sd2e-cli tarball from main repo.
 ##
 set -e
 set -o pipefail
@@ -23,7 +24,12 @@ export green="\e[32m"
 export red="\e[1;31m"
 export reset="\e[0m"
 
+# Parameters
+CONTAINER="sd2e-cli-dev"
+REPO="https://github.com/alejandrox1/sd2e-cli"
+
 # Input parameters
+GET_TAR="false"
 
 # Parse command line arguments.
 while [[ "$#" > 0 ]]; do
@@ -38,6 +44,9 @@ while [[ "$#" > 0 ]]; do
             echo "$(grep "^##" ${BASH_SOURCE[0]} | cut -c 4-)"
             exit 0
             ;;
+        --get-tar)
+            GET_TAR="true"
+            ;;
         *)
             >&2 echo "Unknown command-line option: '${arg}'."
             exit 1
@@ -47,15 +56,22 @@ while [[ "$#" > 0 ]]; do
 done
 
 
-CONTAINER="sd2e-cli-dev"
+if [ "${GET_TAR}" == "true" ]; then
+    echo -e "${red}Getting package tarball...${reset}"
+    curl -L https://raw.githubusercontent.com/sd2e/sd2e-cli/master/sd2e-cloud-cli.tgz \
+        -o sd2e-cloud-cli.tgz && \
+        exit 0
+fi
 
-echo -e "${red}Starting development...${reset}"
+
+echo -e "${red}SD2E-CLI development starting...${reset}"
 docker build --no-cache --force-rm \
     --build-arg UID=$UID \
     --build-arg USER=$USER \
-    --build-arg REPO=https://github.com/alejandrox1/sd2e-cli \ 
+    --build-arg REPO=$REPO \
     -t $CONTAINER . && \
     docker run \
     -v ~/.gitconfig:/home/$USER/.gitconfig \
     -v ~/.git-credentials:/home/$USER/.git-credentials \
+    -w /home/$USER/$(basename $REPO) \
     --rm -it $CONTAINER
