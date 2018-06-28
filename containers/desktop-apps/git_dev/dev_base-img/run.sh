ff86617308eec49b009d1ad82da7f2992718515f
alejandrox1 <alarcj137@gmail.com>
2018-06-28 15:18:10 -0500

    containers/desktop-apps/git_dev/dev_base-img/run.sh cleaned up script + push image
    Signed-off-by: alejandrox1 <alarcj137@gmail.com>

--- containers/desktop-apps/git_dev/dev_base-img/run.sh
+++ containers/desktop-apps/git_dev/dev_base-img/run.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-set -e
-set -o pipefail
-export blue="\e[1;34m"                                                         
-export green="\e[92;1m"                                                           
-export red="\e[1;31m"
-export reset="\e[0m"
 
-CONTAINER="ubuntu18-dev"
+CONTAINER="alejandrox1/ubuntu18-dev"
 
-
-docker build --no-cache --force-rm -t $CONTAINER .
+docker build --no-cache --force-rm -t $CONTAINER . && docker push $CONTAINER
