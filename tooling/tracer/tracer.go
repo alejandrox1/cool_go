4987e4419ea1753b40aa86ecc332236b8f685aa4
Jorge Alarcon Ochoa <alarcj137@gmail.com>
2018-08-21 12:11:17 -0500

    Implement tracer pakcage
    Signed-off-by: Jorge Alarcon Ochoa <alarcj137@gmail.com>

---
+++ tooling/tracer/tracer.go
@@ -0,0 +1,32 @@
+package tracer
+
+import (
+    "fmt"
+    "io"
+)
+
+
+// Tracer is the interface that describes an object capable of tracing events
+// throughout a service.
+type Tracer interface {
+    Trace(...interface{})
+}
+
+
+// tracer implements the Tracer interface by writing to an io.Writer.
+type tracer struct {
+    out io.Writer
+}
+
+
+// New creates a new Tracer that will write the output to the specified
+// io.Writer.
+func New(w io.Writer) Tracer {
+    return &tracer{out: w}
+}
+
+// Trace writes the arguments to this tracer to the io.Writer.
+func (t *tracer) Trace (a ...interface{}) {
+    fmt.Fprint(t.out, a...)
+    fmt.Fprintln(t.out)
+}
